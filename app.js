35b15c62e3812abec4df113aa2117e9f47e0211c
root <root@ip-172-31-24-139.us-east-2.compute.internal>
2020-05-08 14:27:15 +0000

    Added files and changes required for deployment to heroku

---
+++ app.js
@@ -0,0 +1,3 @@
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
